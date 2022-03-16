d1d2247e8470d243c077ed5fa663068fd00e5128
kostya-alch <kostafrompiter@gmail.com>
2022-03-17 00:25:28 +0300

    register validate and hash pass

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const config = require('config');
+const authRouter = require('./routes/auth.routes');
 
 const app = express();
 
 const PORT = config.get('serverPort');
+
+app.use(express.json({ extended: true }));
+app.use('/api/auth', authRouter);
 
 const start = async () => {
    try {
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,26 +1,43 @@
 const { Router } = require('express');
 const router = Router();
 const User = require('../models/User');
+const { check, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
 
-router.post('/registration', async (req, res) => {
-   try {
-      const { email, password } = req.body;
-      const isUsed = await User.findOne({ email });
+router.post(
+   '/registration',
+   [
+      check('email', 'Некорректный ввод').isEmail(),
+      check('password', 'Некорректный ввод').isLength({ min: 6 }),
+   ],
+   async (req, res) => {
+      try {
+         const errors = validationResult(req);
+         if (!errors.isEmpty()) {
+            return res.status(400).json({
+               errors: errors.array(),
+               message: 'Некорректные данные при регистрации',
+            });
+         }
+         const { email, password } = req.body;
+         const isUsed = await User.findOne({ email });
 
-      if (isUsed) {
-         return res.status(300).json({
-            message: 'Данный email уже занят, попробуйте другой',
+         if (isUsed) {
+            return res.status(300).json({
+               message: 'Данный email уже занят, попробуйте другой',
+            });
+         }
+         const hashedPassword = await bcrypt.hash(password, 12);
+         const user = new User({
+            email,
+            password: hashedPassword,
          });
+         await user.save();
+         res.status(201).json({ message: 'Пользователь успешно создан' });
+      } catch (error) {
+         console.log(error);
       }
-      const user = new User({
-         email,
-         password,
-      });
-      await user.save();
-      res.status(201).json({ message: 'Пользователь успешно создан' });
-   } catch (error) {
-      console.log(error);
    }
-});
+);
 
 module.exports = router;
