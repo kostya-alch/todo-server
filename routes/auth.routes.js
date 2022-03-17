4c74cf8919b74728d57963c22b383bc198e427e2
kostya-alch <kostafrompiter@gmail.com>
2022-03-17 18:27:44 +0300

    login route logic

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -3,6 +3,7 @@ const router = Router();
 const User = require('../models/User');
 const { check, validationResult } = require('express-validator');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 router.post(
    '/registration',
@@ -34,6 +35,51 @@ router.post(
          });
          await user.save();
          res.status(201).json({ message: 'Пользователь успешно создан' });
+      } catch (error) {
+         console.log(error);
+      }
+   }
+);
+
+router.post(
+   '/login',
+   [
+      check('email', 'Некорректный ввод').isEmail(),
+      check('password', 'Некорректный ввод').exists(),
+   ],
+   async (req, res) => {
+      try {
+         const errors = validationResult(req);
+         if (!errors.isEmpty()) {
+            return res.status(400).json({
+               errors: errors.array(),
+               message: 'Некорректные данные при регистрации',
+            });
+         }
+         const { email, password } = req.body;
+
+         const user = await User.findOne({ email });
+         if (!user) {
+            return res.json({ message: 'Такого email не существует' });
+         }
+
+         const isMatchPassword = bcrypt.compare(password, user.password);
+
+         if (!isMatchPassword) {
+            return res.json({ message: 'Пароли не совпадают' });
+         }
+         const jwtSecret = 'dadadadadadmadadajdapdamfac';
+
+         const token = jwt.sign(
+            {
+               userId: user._id,
+            },
+            jwtSecret,
+            {
+               expiresIn: '1h',
+            }
+         );
+         res.json({ token, userId: user._id });
       } catch (error) {
          console.log(error);
       }
