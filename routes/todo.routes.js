388f8d1978f00a006aaab73e5ae8bc5977ca8292
kostya-alch <kostafrompiter@gmail.com>
2022-03-18 22:46:14 +0300

    route for todos

--- routes/todo.routes.js
+++ routes/todo.routes.js
@@ -20,4 +20,46 @@ router.post('/add', async (req, res) => {
    }
 });
 
+router.get('/', async (req, res) => {
+   try {
+      const { userId } = req.query;
+
+      const todo = await Todo.find({ owner: userId });
+      res.json(todo);
+   } catch (error) {
+      console.log(error);
+   }
+});
+
+router.delete('/delete/:id', async (req, res) => {
+   try {
+      const todo = await Todo.findOneAndDelete({ _id: req.params.id });
+      res.json(todo);
+   } catch (error) {
+      console.log(error);
+   }
+});
+
+router.put('/complete/:id', async (req, res) => {
+   try {
+      const todo = await Todo.findOne({ _id: req.params.id });
+      todo.completed = !todo.completed;
+      await todo.save();
+      res.json(todo);
+   } catch (error) {
+      console.log(error);
+   }
+});
+
+router.put('/important/:id', async (req, res) => {
+   try {
+      const todo = await Todo.findOne({ _id: req.params.id });
+      todo.important = !todo.important;
+      await todo.save();
+      res.json(todo);
+   } catch (error) {
+      console.log(error);
+   }
+});
+
 module.exports = router;
