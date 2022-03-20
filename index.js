02b569c47d28decddee4b54793e60aeebee06071
kostya-alch <kostafrompiter@gmail.com>
2022-03-20 21:33:05 +0300

    deploy

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ require('dotenv').config();
 const config = require('config');
 const authRouter = require('./routes/auth.routes');
 const todoRouter = require('./routes/todo.routes');
-const cors = require('cors');
+const corss = require('cors');
 const app = express();
 
 const PORT = process.env.PORT || config.get('serverPort');
-app.use(cors());
+app.use(corss());
 app.use(express.json({ extended: true }));
 app.use('/api/auth', authRouter);
 app.use('/api/todo', todoRouter);
